6bacd35edf43b6e0836cac4f2b73f70bcf0a3734
Jayesh Tanna <tanna.jay90@gmail.com>
2019-08-26 22:22:12 +0530

    used configuration for token service

--- CoreWebAPI.JWTAuthentication/CoreWebAPI.JWTAuthentication/Services/JWTTokenAuthService.cs
+++ CoreWebAPI.JWTAuthentication/CoreWebAPI.JWTAuthentication/Services/JWTTokenAuthService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -8,9 +9,18 @@ namespace CoreWebAPI.JWTAuthentication.Services
 {
     public class JWTTokenAuthService : IAuthService
     {
-        private const string SecurityAlgorithm = SecurityAlgorithms.HmacSha256Signature;
-        private const int ExpiryInMinutes = 10080;
-        private string SecretKey = "TW9zaGVFcmV6UHJpdmF0ZUtleQ==";
+        private string SecurityAlgorithm { get; }
+        private int ExpiryInMinutes { get; }
+        private string SecretKey { get; }
+        private readonly IConfiguration configuration;
+
+        public JWTTokenAuthService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            SecretKey = this.configuration["JWTTokenService:SecretKey"];
+            ExpiryInMinutes = Convert.ToInt32(this.configuration["JWTTokenService:ExpiryInMinutes"]);
+            SecurityAlgorithm = this.configuration["JWTTokenService:SecurityAlgorithm"];
+        }
 
         public bool IsTokenValid(string token)
         {
@@ -40,8 +50,8 @@ namespace CoreWebAPI.JWTAuthentication.Services
                 SigningCredentials = new SigningCredentials(GetSymmetricSecurityKey(), SecurityAlgorithm)
             };
 
-            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
-            SecurityToken securityToken = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
             string token = jwtSecurityTokenHandler.WriteToken(securityToken);
 
             return token;
@@ -50,7 +60,7 @@ namespace CoreWebAPI.JWTAuthentication.Services
         public IEnumerable<Claim> GetTokenClaims(string token)
         {
             if (string.IsNullOrEmpty(token))
-                throw new ArgumentException("Given token is null or empty.");
+                throw new ArgumentException("Invalid token");
 
             TokenValidationParameters tokenValidationParameters = GetTokenValidationParameters();
 
